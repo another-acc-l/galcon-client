659d331fee86edda93aa36000a870d339e1d1477
Dmytro <just.another.dev.acc@gmail.com>
2025-06-25 13:19:21 +0300

    added endScene

---
+++ Assets/Scripts/UI/EndGameUI.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class EndGameUI : MonoBehaviour
+{
+    [SerializeField] UnityEngine.UI.Text resultLabel;
+    [SerializeField] Button btnMainMenu;
+
+    void Start()
+    {
+        resultLabel.text = GameManager.LocalPlayerWon ? "You Win!" : "You Lose.";
+
+        btnMainMenu.onClick.AddListener(MoveToMainMenu);
+        //btnMainMenu.onClick.AddListener(() =>
+        //{
+        //    ConnectionManager.Instance?.Shutdown();          // відʼєднати NGO
+        //    SceneManager.LoadScene("MainMenuScene");
+        //});
+    }
+
+    void MoveToMainMenu()
+    {
+        ConnectionManager.Instance?.Shutdown();        
+        SceneManager.LoadScene("MainMenuScene");
+    }
+}
